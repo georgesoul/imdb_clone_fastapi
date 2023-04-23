f1afb713028fa51c9044903f368a1d179f0351bb
georgesouliotis <george_astrobip96@hotmail.com>
2023-04-23 21:00:50 +0300

    data validation with pydantic

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
+from typing import Optional, List
 from fastapi import FastAPI, status, HTTPException, Response, Depends
 from fastapi.params import Body
 import psycopg2
-from pydantic import BaseModel
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 import models
+import schemas
 from database import engine, SessionLocal, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
     
-
 # while True:
 
 #     try:
@@ -36,16 +31,16 @@ class Post(BaseModel):
 def root():
     return {"message": "It works!"}
 
-@app.get('/posts', status_code=status.HTTP_201_CREATED)
+@app.get('/posts', response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM posts; """)
     # posts = cursor.fetchall()
     
     posts = db.query(models.Post).all()
-    return { "data": posts }
+    return posts
 
-@app.post('/posts')
-async def create_post(post: Post, db: Session = Depends(get_db)):
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -57,9 +52,9 @@ async def create_post(post: Post, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
     
-    return {"data": new_post}
+    return new_post
 
-@app.get('/posts/{id}')
+@app.get('/posts/{id}', response_model=schemas.Post)
 async def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, str(id))
     # post = cursor.fetchone()
@@ -68,7 +63,7 @@ async def get_post(id: int, db: Session = Depends(get_db)):
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} was not found")
-    return {"data": post}
+    return post
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int, db: Session = Depends(get_db)):
@@ -87,8 +82,8 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put('/posts/{id}')
-async def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+@app.put('/posts/{id}', response_model=schemas.Post)
+async def update_post(id: int, post: schemas.PostUpdate, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, 
     #                (post.title, post.content, post.published, str(id)))
     
@@ -104,6 +99,6 @@ async def update_post(id: int, post: Post, db: Session = Depends(get_db)):
     updated_post = post_query.update(post.dict(), synchronize_session=False)
     db.commit()
     
-    return {"data": post_query.first()}
+    return post_query.first()
     
     
