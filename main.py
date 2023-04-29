ad1c1c6f243e517a9a2a84d2f5fff5e0c5a387e5
georgesouliotis <george_astrobip96@hotmail.com>
2023-04-29 18:36:21 +0300

    deploy 1

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from fastapi import FastAPI
 from app import models
 # from app.database import engine
 from app.routes import user, post, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
+# Initialize our db based on our sqlalchemy models - deactivated since we introduced alembic
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*"
+    # "https://www.google.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
